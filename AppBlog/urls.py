aaa32c9d01bddd20221a819ee0ca455a4eb5ecbd
ncode90 <nahuel_regueira@hotmail.com>
2022-04-05 01:19:44 -0300

    forms I

--- AppBlog/urls.py
+++ AppBlog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('users', views.users, name="Users"),
     path('profiles', views.profiles, name="Profiles"),
     path('posts', views.posts, name="Posts"),
+    path('addUser', views.addUser, name="AddUser"),
+    path('addProfile', views.addProfile, name="AddProfile"),
+    path('addPost', views.addPost, name="AddPost"),
 ]
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from AppBlog.models import Post, Profile, User
 
 def home(request):
       return render(request, "home.html")
@@ -11,3 +12,24 @@ def profiles(request):
 
 def posts(request):
       return render(request, "posts.html")
+
+def addUser(request):
+      if request.method == "POST":
+            user = User(username=request.POST['username'], password=request.POST['password'], email=request.POST['email'])
+            user.save()
+            return render(request, "home.html")
+      return render(request, "addUser.html")
+
+def addProfile(request):
+      if request.method == "POST":
+            user = Profile(name=request.POST['name'], description=request.POST['description'], image=request.POST['image'], website=request.POST['website'], email=request.POST['email'], password=request.POST['password'])
+            user.save()
+            return render(request, "home.html")
+      return render(request, "addProfile.html")
+
+def addPost(request):
+      if request.method == "POST":
+            user = Post(title=request.POST['title'], subtitle=request.POST['subtitle'], body=request.POST['body'], author=request.POST['author'], date=request.POST['date'], image=request.POST['image'])
+            user.save()
+            return render(request, "home.html")
+      return render(request, "addPost.html")
